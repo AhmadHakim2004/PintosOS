8fac3a57afe182da45ddc4df89ea444d76360554
AhmadHakim2004 <hakimahmad2004@gmail.com>
2024-06-27 07:24:01 -0400

    Map Handlers

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,6 +5,20 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+static void halt_handler (void);
+static void exit_handler (int);
+static int exec_handler (char *);
+static int wait_handler (int);
+static bool create_handler (char *, unsigned);
+static bool remove_handler (char *);
+static int open_handler (char *);
+static int filesize_handler (int);
+static int read_handler (int, char *, unsigned);
+static int write_handler (int, char *, unsigned);
+static void seek_handler (int, unsigned);
+static unsigned tell_handler (int);
+static void close_handler (int);
+static bool is_valid_pointer (void *);
 
 void
 syscall_init (void) 
@@ -13,8 +27,142 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call!\n");
+  int *interrupt_code = (int *)f->esp;
+  void *arg1 = f->esp+4;
+  void *arg2 = f->esp+8;
+  void *arg3 = f->esp+12;
+
+  switch (*interrupt_code)
+    {
+      case 0:
+        halt_handler ();
+        break;
+      case 1:
+        exit_handler (*(int *)arg1);
+        break;
+      case 2:
+        f->eax = exec_handler (*(char **)arg1);
+        break;
+      case 3:
+        f->eax = wait_handler (*(int *)arg1);
+        break;
+      case 4:
+        f->eax = create_handler (*(char **)arg1, *(unsigned *)arg2);
+        break;
+      case 5:
+        f->eax = remove_handler (*(char **)arg1);
+        break;
+      case 6:
+        f->eax = open_handler (*(char **)arg1);
+        break;
+      case 7:
+        f->eax = filesize_handler (*(int *)arg1);
+        break;
+      case 8:
+        f->eax = read_handler (*(int *)arg1, *(char **)arg2, *(unsigned *)arg3);
+        break;
+      case 9:
+        f->eax = write_handler (*(int *)arg1, *(char **)arg2, *(unsigned *)arg3);
+        break;
+      case 10:
+        seek_handler (*(int *)arg1, *(unsigned *)arg2);
+        break;
+      case 11:
+        f->eax = tell_handler (*(int *)arg1);
+        break;
+      case 12:
+        close_handler (*(int *)arg1);
+        break;
+      default:
+        exit_handler (-1);
+    }
+}
+
+static void 
+halt_handler ()
+{
+  printf("halt_handler not implemented yet");
+}
+
+static void 
+exit_handler (int status)
+{  
   thread_exit ();
+}
+
+static int 
+exec_handler (char *file)
+{
+  printf("exec_handler not implemented yet");
+  return -1;
+}
+
+static int 
+wait_handler (int pid)
+{
+  printf("wait_handler not implemented yet");
+  return -1;
+}
+
+static bool 
+create_handler (char *file, unsigned initial_size)
+{
+  printf("create_handler not implemented yet");
+  return false;
+}
+
+static bool 
+remove_handler (char *file)
+{
+  printf("remove_handler not implemented yet");
+  return false;
+}
+
+static int 
+open_handler (char *file)
+{
+  printf("open_handler not implemented yet");
+  return -1;
+}
+
+static int 
+filesize_handler (int fd)
+{
+  printf("filesize_handler not implemented yet");
+  return -1;
+}
+
+static int 
+read_handler (int fd, char *buffer, unsigned length)
+{
+  printf("read_handler not implemented yet");
+  return -1;
+}
+
+static int 
+write_handler (int fd, char *buffer, unsigned length)
+{
+  printf("write_handler not implemented yet");
+  return -1;
+}
+
+static void 
+seek_handler (int fd, unsigned postition)
+{
+  printf("seek_handler not implemented yet");
+}
+
+static unsigned 
+tell_handler (int fd)
+{
+  printf("tell_handler not implemented yet");
+  return -1;
+}
+
+static void 
+close_handler (int fd)
+{
+  printf("close_handler not implemented yet");
 }
