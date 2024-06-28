36a0cb1a93fe7ff5c560281348202902a695e2bd
Asad <hasad514@gmail.com>
2024-06-27 20:39:10 -0400

    get file from fd function in process.c

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -8,6 +8,7 @@ int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
 void init_process_control_block(struct thread *t);
+struct file* get_file_from_fd(int fd);
 
 /* Process Control Block Struct */
 struct fds
