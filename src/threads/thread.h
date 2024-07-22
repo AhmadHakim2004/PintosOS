116809a95a3518cb55716e02bdbf1542fdbbef44
Asad <hasad514@gmail.com>
2024-07-22 10:00:16 -0400

    Implemented stack growth

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -26,6 +26,10 @@ typedef int tid_t;
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
 
+
+/* Max stack size */
+#define MAX_STACK_SIZE 0x800000
+
 /* A kernel thread or user process.
 
    Each thread structure is stored in its own 4 kB page.  The
